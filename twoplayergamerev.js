9ead5b4d45f911c00f49a0885222ca6a295ab7d7
achandi <chandi.ajit@gmail.com>
2016-05-03 12:15:50 -0400

    Refactored to Jquery

--- twoplayergamerev.js
+++ twoplayergamerev.js
@@ -1,55 +1,35 @@
-var bankRoll = 100;
+$(document).ready(function() {
 
-var playerBet = function() { 
-   var bet = prompt("Place a bet between 5 and 10 dollars");
-   if (playerBet > 10 || playerBet < 5) {
-    console.log("Wrong number. Place a bet between 5 and 10");
-    return bet = prompt("Place a bet between 5 and 10 dollars");
- } else {
-    console.log("you placed a bet of $" + bet);
-    return bet;
- }
+  var bankRoll = 100;
 
-};
+  $("#bet").on('click', function() {
+  var bet = $("#Gamebet").val();
+  var guess = $("#playerGuess").val();
+  game(guess, computerGuess(), bet);
+  });
 
-var playerGuess = function () {
-    var guess = prompt("Now, guess a number between 1 and 10");
-  if (guess > 10 || guess < 1) {
-    console.log("Error, guess a number between 1 and 10");
-    return guess = prompt("Guess a number between 1 and 10");
-} else { 
-    console.log("your guess is " + guess); 
-    return guess;
-}
-};
-
-var computerGuess = function() {
+  var computerGuess = function() {
   var cpuGuess = Math.floor((Math.random() * 10)+ 1);
-  console.log("the computer guessed " + cpuGuess);
   return cpuGuess;
-};
+  };
 
-var game = function(choice1, choice2)  { 
-  if (choice1 == choice2) {
-     bankRoll += parseInt(currentBet);
-     return ("you win $ " + currentBet + " and now have $" + bankRoll);
+  var game = function(guess, answer, currentBet)  {
+    if (currentBet > 10 || currentBet < 5) {
+      $('.validator1').text("Wrong bet. Place a bet between 5 and 10");
+  } else if (bankRoll < currentBet) {
+      $('.validator2').text("You do not have enough money left to bet this amount." + "Your balance is $" + bankRoll);
+  } else if (guess > 10 || guess < 1) {
+      $('.validator3').text("Error, guess a number between 1 and 10");
   } else {
-     bankRoll -= currentBet;
-     return ("you lose $ " + currentBet + " and now have $" + bankRoll);
-  }
+      $(".answer").text("The computer answered " + answer);
+        if (guess == answer) {
+          bankRoll += parseInt(currentBet);
+          $('.gameresult').text("you win $ " + currentBet + " and now have $" + bankRoll);
+      } else {
+        bankRoll -= currentBet;
+        $('.gameresult').text("you lose $ " + currentBet + " and now have $" + bankRoll);
+      }
+    }
+  };
+});
 
-};
-
-var currentBet = playerBet();
-
-while (bankRoll >= currentBet) {
-  var guess = playerGuess();
-  var computer = computerGuess();
-  console.log(game(guess,computer));
-  currentBet = playerBet();
-  var playAgain = prompt("do you want to quit? (type q)");
-  console.log(playAgain);
-  if (playAgain == "q"){
-      break;
-  }
-}
